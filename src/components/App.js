3f287fde8de5590bd493421b00a11b8dc13a9dcc
TheJis <ing.jesithgarcia@gmail.com>
2021-09-22 00:24:51 -0500

    implementacion api unsplash busqueda imagen random

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,76 @@
+import React from 'react'
 
 import './App.css';
 import logo from '../img/logo.png'
 import PiePagina from './PiePagina'
 
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Button, Form, Input } from 'reactstrap';
+import { Container } from 'reactstrap';
 import { FaSearch } from 'react-icons/fa'
 
-function App() {
-  return (
-    <div className="container">
-      <div className="App">
-        <Form>
-          <img src={logo} alt="logo"/>
-          <div className="search">
-            <div className="icon"><FaSearch /></div>
-            <Input className="searchBar" type="search" name="searchBox" id="searchBox" placeholder="Search Images..." />
-          </div>
-          <br />
-          <Button color="success"><b>Search</b></Button> 
-        </Form>
-      
+const clientID = "H3nbGe6qvZtX7RjMpbcfYsyQ305vxwFLlEtTodIO-mM"
+const endpoint = "https://api.unsplash.com/search/photos"
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.query = "";
+    this.trackQueryValue = this.trackQueryValue.bind(this);
+    this.search = this.search.bind(this);
+
+    this.state = {
+      images: []
+    }
+  }
+  search(){
+    fetch(`${endpoint}?query=${this.query}&client_id=${clientID}`)
+    .then(response=>{
+      return response.json()
+    }).then(jsonResponse=>{
+      console.log(jsonResponse);
+      this.setState({
+        images: jsonResponse.results
+      })
+    })
+  }
+
+  trackQueryValue(ev){
+    this.query = ev.target.value;
+  }
+
+  images(){
+    return this.state.images.map(image=>{
+      return <img src={image.urls.thumb} key={image.id} alt="" />
+    })
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <div className="App">
+          <Form>
+            <img src={logo} alt="logo"/>
+            <div className="search">
+              <div className="icon"><FaSearch /></div>
+              <Input className="input" type="text" onChange={this.trackQueryValue} placeholder="  Search Images..." />
+            </div>
+            <br />
+            <Button onClick={this.search} onKeyDown={this.search} color="success"><b>Search</b></Button> 
+          </Form>
+        
+        </div>
+
+        <Container>
+
+              {this.images()}
+
+        </Container>
+  
+
+        <PiePagina className="piepagina"/>
       </div>
-      <PiePagina className="piepagina"/>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/components/PiePagina.js
+++ src/components/PiePagina.js
@@ -1,10 +1,18 @@
 //import React from 'react';
 import { FaFacebook, FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa'
+import { BrowserRouter, Link } from 'react-router-dom'
 
 function PiePagina(){
 	return(
 		<div className="container-pie">
-		 	<h5><b>T h e J i s</b> &copy; <FaGithub /> <FaFacebook /> <FaTwitter /> <a href="https://www.linkedin.com/in/jesith-garc%C3%ADa-ing15/"> <FaLinkedin /></a></h5>
+		 	<h5><b>T h e J i s</b> &copy;
+		 	<BrowserRouter> 
+		 	<Link to="https://github.com"><FaGithub /></Link>
+		 	<Link to="https://facebook.com"><FaFacebook /></Link> 
+		 	<Link to="https://twitter.com"><FaTwitter /></Link> 
+		 	<Link to="https://www.linkedin.com/in/jesith-garc%C3%ADa-ing15/"><FaLinkedin /></Link>
+		 	</BrowserRouter>
+		 	</h5>
 		</div>
 	);
 }
