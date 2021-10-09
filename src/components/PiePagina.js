56b0a0dd0c56a1689e9a25d6e1f4ef0ac3706a72
TheJis <ing.jesithgarcia@gmail.com>
2021-10-09 16:38:59 -0500

    actualizando enlaces en el footer

--- src/components/PiePagina.js
+++ src/components/PiePagina.js
@@ -1,16 +1,21 @@
 //import React from 'react';
-import { FaFacebook, FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa'
-import { BrowserRouter, Link } from 'react-router-dom'
+import { FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa'
+// import { BrowserRouter, Link } from 'react-router-dom'
 
 function PiePagina(){
 	return(
 		<div className="container-pie">
 		 	<h5><b>T h e J i s</b> &copy;
-		 	<BrowserRouter> 
-		 	<Link to="https://github.com/ing-thejis"><FaGithub /></Link>
-		 	<Link to="https://twitter.com/ingGarc1a"><FaTwitter /></Link> 
-		 	<Link to="https://www.linkedin.com/in/jesith-garc%C3%ADa-ing15/"><FaLinkedin /></Link>
-		 	</BrowserRouter>
+		 	
+		 	
+		 	<a href="https://github.com/ing-thejis"><FaGithub /></a>
+		 	<a href="https://twitter.com/ingGarc1a"><FaTwitter /></a> 
+		 	<a href="https://www.linkedin.com/in/jesith-garc%C3%ADa-ing15/"><FaLinkedin /></a>
+
+			{/*<Link to="https://github.com/ing-thejis"><FaGithub /></Link>*/}
+		 	{/*<Link to="https://twitter.com/ingGarc1a"><FaTwitter /></Link> 
+		 	<Link to="https://www.linkedin.com/in/jesith-garc%C3%ADa-ing15/"><FaLinkedin /></Link>*/}
+		 	
 		 	</h5>
 		</div>
 	);
